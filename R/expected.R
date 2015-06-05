8ef0e4433604229d83a9f105047fe0b3e7fa9c1b
merlinoa <merlinoa88@gmail.com>
2015-06-05 14:10:15 -0400

    simplified function with new validate and trim utilities

--- R/expected.R
+++ R/expected.R
@@ -23,23 +23,19 @@ setGeneric("expected",
 #' 
 #' @export
 #' @examples
-#' expected(T_x(), t_ = 3)
-setMethod("expected", signature("T_x"), function(object, t_ = NULL, m_ = 0) {
-  # isolate all q_x >= T_x@x_ 
-  q_x <- object@q_x[object@x >= object@x_]
+#' expected(LifeTable(), x_ = 0, t_ = 3)
+setMethod("expected", signature("LifeTable"), function(object, 
+                                                       x_ = object@x[1], 
+                                                       t_ = max(object@x) - object@x[1], 
+                                                       m_ = 0) {
+  validate_x_(object, x_ = x_)
+  validate_t_(object, x_ = x_, t_ = t_)
+  validate_m_(object, x_ = x_, t_ = t_, m_ = m_)
   
-  # set t if not specified
-  if (is.null(t_)) {
-    t_ <- length(q_x)
-  } else {
-    stopifnot(t_ >= 1)
-    stopifnot(m_ >= 0)
-    # stop if life_table not large enough; t_ + m_ exceeds remaining q_x values
-    stopifnot(length(q_x) >= m_ + t_)
-  }
-  
+  q_x <- trim_table(object, slot_ = "q_x", x_ = x_, t_ = t_, m_ = m_)
+ 
   # calculate curtate life expectancy
-  sum(cumprod(1 - q_x[(1 + m_):(t_ + m_)]))
+  sum(cumprod(1 - q_x))
 })
 
 #' expected
@@ -54,38 +50,30 @@ setMethod("expected", signature("T_x"), function(object, t_ = NULL, m_ = 0) {
 #' @export
 #' @examples
 #' # 3 year term insurance with a payable amount or 4 
-#' expected(Z_x(payable = 4), t_ = 3) 
+#' expected(Z_x(), t_ = 3) 
 #' # 3 year term insurance effective in 1 year
-#' expected(Z_x(), t_ = 3, m_ = 1)
-setMethod("expected", signature("Z_x"), function(object, t_ = NULL, m_ = 0) {
-  # isolate all q_x >= T_x@x_ 
-  q_x <- object@q_x[object@x >= object@x_]
-  i <- object@i[object@x >= object@x_]
+#' expected(Z_x(benefit = 1, x_ = 0), t_ = 3, m_ = 1)
+setMethod("expected", signature("Z_x"), function(object, t_ = max(object@x) - object@x_, m_ = 0) {
+
+  validate_t_(object, x_ = object@x_, t_ = t_)
+  validate_m_(object, x_ = object@x_, t_ = t_, m_ = m_)
   
-  # set t if not specified
-  if (is.null(t_)) {
-    t_ <- length(q_x)
-  } else {
-    stopifnot(t_ >= 1)
-    stopifnot(m_ >= 0)
-    # stop if life_table not large enough; t_ + m_ exceeds remaining q_x values
-    stopifnot(length(q_x) >= m_ + t_)
-  }
+  q_x <- trim_table(object, slot_ = "q_x", x_ = object@x_, t_ = m_ + t_)
+  i <- trim_table(object, slot_ = "i", x_ = object@x_, t_ = m_ + t_)
   
+  index <- (m_ + 1):(m_ + t_)
   # isolate applicable interest discount
-  x_trend <- 1 + object@i
+  x_trend <- 1 + i
   x_discount <- 1 / x_trend
-  discount <- cumprod(x_discount[1:(m_ + t_)])
-  discount <- discount[(m_ + 1):(m_ + t_)]
+  discount <- cumprod(x_discount)
   
   # isolate applicatbe mortality discount
-  tp_x <- c(1, cumprod(1 - q_x[1:(t_ + m_ - 1)]))
-  tp_x <- tp_x[(m_ + 1):(t_ + m_)]
+  tp_x <- c(1, cumprod(1 - q_x[-length(q_x)]))
   
   # calculate the present value of Z_x
-  z <- tp_x * discount * q_x[(m_ + 1):(t_ + m_)]
-  z <- sum(z)
-  z * object@payable
+  out <- tp_x[index] * discount[index] * q_x[index]
+  out <- sum(out)
+  out * object@benefit
 })
 
 #' expected
