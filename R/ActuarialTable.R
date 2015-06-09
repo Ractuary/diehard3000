a46c464bfe91465efad6551311cd3b7751bda1a0
merlinoa <merlinoa88@gmail.com>
2015-06-09 12:04:26 -0400

    documentation

--- R/ActuarialTable.R
+++ R/ActuarialTable.R
@@ -32,42 +32,8 @@ ActuarialTable <- setClass("ActuarialTable",
 )
 
 
-#' Z_x
-#' 
-#' The present value random variable for a life contingent 
-#' insurance payment payable at the end of the year of death
-#' 
-#' @include ActuarialTable.R
-#' @slot x_ x value for individual
-#' @slot benefit the amount payable at the end of the year of death
-#' 
-#' @name Z_x-class
-#' @rdname Z_x-class
-#' @export Z_x
-Z_x <- setClass("Z_x",
-         contains = "ActuarialTable",
-         slots = list(x_ = "numeric",
-                      benefit = "numeric"),
-         prototype = prototype(x_ = 2, benefit = 10),
-         validity = function(object) identical(length(object@x_), 1L)
-       )
 
 
-#' Y_x
-#'
-#' present value random variable for life contingent
-#' annuity payments
-#'
-#' @include ActuarialTable.R
-#' @slot x_ x value for individual
-#' @slot benefit the annuity benefit
-#' 
-#' @name Y_x-class
-#' @rdname Y_x-class
-#' @export Y_x
-Y_x <- setClass("Y_x",
-         contains = "Z_x",
-         slots = list(annuity = "numeric"),
-         prototype = prototype(annuity = 1),
-         validity = function(object) identical(length(object@annuity), 1L)
-)
+
+
+
