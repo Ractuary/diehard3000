edff2a99ee5faebdbf9924451317362fc400c46a
merlinoa88@gmail.com <merlinoa88@gmail.com>
2015-06-11 20:33:38 -0400

    fixed m_ for real this time

--- R/utils.R
+++ R/utils.R
@@ -11,8 +11,7 @@
 #' 
 #' @export
 #' @examples 
-#' object <- LifeTable()
-#' trim_table(object, slot_ = "q_x", x_ = 2, t_ = 3)
+#' trim_table(LifeTable(), slot_ = "q_x", x_ = 2, t_ = 3, m_ = 2)
 trim_table <- function(object,
                        slot_,
                        x_ = object[1], 
@@ -20,9 +19,9 @@ trim_table <- function(object,
                        m_ = 0) {
   trim <- slot(object, slot_)
   trim <- trim[object@x >= x_]
-  trim <- trim[1:t_]
+  trim <- trim[1:(t_ + m_)]
   if (m_ > 0) {
-    trim[-(1:m_)]
+    trim <- trim[-(1:m_)]
   }
   trim
 }
@@ -34,7 +33,7 @@ trim_table <- function(object,
 #' returns the probability of death in each x for a percon age x_ 
 tp_x8q_x <- function(object) {
   # isolate all q_x >= Z_x@x_ 
-  q_x <- trim_table(object, slot_ = "q_x", x_ = object@x_, t_ = object@t_, m_ = object@m_)
+  q_x <- trim_table(object, slot_ = "q_x", x_ = object@x_, t_ = object@t_ + object@m_)
 
   # prob of surviving to each x
   tp_x <- sapply(seq_along(q_x), function(j) p_x(object = object, x = object@x_, t_ = j))
