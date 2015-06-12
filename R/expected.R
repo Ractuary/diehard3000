2944b7248268db06790f3864ac4601fce2b24e50
merlinoa <merlinoa88@gmail.com>
2015-06-12 14:16:35 -0400

    adjustments from Y_x and Z_x change to Insuree

--- R/expected.R
+++ R/expected.R
@@ -37,20 +37,20 @@ setMethod("expected", signature("LifeTable"), function(object,
 
 #' expected
 #' 
-#' expected present value of an insurance payment \code{Z_x}.  \code{Z_x}
-#' is discounted for interest and mortality. 
+#' expected present value (discounted for interest and mortality) 
+#' of a single death benefit of 1.
 #' 
-#' @param object object of class Z_x
+#' @param object object of class Insuree
 #' @param t_ t
 #' @param m_ m
 #' 
 #' @export
 #' @examples
 #' # 3 year term insurance with benefit of 4
-#' expected(Z_x(t_ = 3, benefit = 4)) 
+#' expected(Insuree(t_ = 3, benefit = 4)) 
 #' # 3 year term insurance effective in 1 year
-#' expected(Z_x(benefit = 1, x_ = 0, m_ = 1, t_ = 3))
-setMethod("expected", signature("Z_x"), function(object) {
+#' expected(Insuree(benefit = 1, x_ = 0, m_ = 1, t_ = 3))
+setMethod("expected", signature("Insuree"), function(object) {
   
   q_x <- trim_table(object, slot_ = "q_x", x_ = object@x_, t_ = object@m_ + object@t_)
   i <- trim_table(object, slot_ = "i", x_ = object@x_, t_ = object@m_ + object@t_)
@@ -65,24 +65,5 @@ setMethod("expected", signature("Z_x"), function(object) {
   index <- (object@m_ + 1):(object@m_ + object@t_)
   out <- tp_x[index] * discount[index] * q_x[index]
   out <- sum(out)
-  out * object@benefit
-})
-
-#' expected
-#' 
-#' expected value of \code{Y_x}
-#' 
-#' @param object object of class Z_x
-#' @param t_ t
-#' @param m_ m
-#' @param payable character string indicating the time the annuity
-#' payments are made.  Can either be "beg" or "end".  Default value
-#' set to "end".
-#' 
-#' @export
-#' @examples
-#' expected(new("Y_x"), t_ = 3)
-setMethod("expected", signature("Y_x"), function(object, t_ = NULL, m_ = 0, payable = "end") {
-  # need to think about this
-  # might want to remove Y_x class and calculte expected Y_x in Z_x expected()
+  out
 })
