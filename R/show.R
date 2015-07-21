5ed3c206c0d9118a6e8c5bacb5a414ba0b7ff998
merlinoa <merlinoa88@gmail.com>
2015-07-21 16:28:39 -0400

    making show methods print automatically

--- R/show.R
+++ R/show.R
@@ -7,9 +7,13 @@
 #' show(LifeTable()) 
 setMethod("show", "LifeTable",
   function(object) {
-    data.frame(x = object@x, 
-               t = object@t, 
-               q_x = object@q_x)
+    print(
+      data.frame(
+        x = object@x, 
+        t = object@t, 
+        q_x = object@q_x
+      )
+    )
   }
 )
 
@@ -21,12 +25,16 @@ setMethod("show", "LifeTable",
 #' @examples 
 #' show(ActuarialTable()) 
 setMethod("show", "ActuarialTable",
-          function(object) {
-            data.frame(x = object@x, 
-                       t = object@t, 
-                       q_x = object@q_x,
-                       i = object@i)
-          }
+  function(object) {
+    print(
+      data.frame(
+        x = object@x, 
+        t = object@t, 
+        q_x = object@q_x,
+        i = object@i
+      )
+    )
+  }
 )
 
 #' show Insuree method
@@ -38,23 +46,24 @@ setMethod("show", "ActuarialTable",
 #' show(Insuree())
 #' show(Insuree(x_ = 2.5, t_ = 3, benefit = c(1, 1, 4, 1))) 
 setMethod("show", "Insuree",
-          function(object) {
-            trim_m_ <- object[object@x_, object@m_]
-            trim_t_ <- object[object@x_ + object@m_, object@t_]
-            lt <- data.frame(x = c(trim_m_@x, trim_t_@x),
-                             t = c(trim_m_@t, trim_t_@t),
-                             q_x = c(trim_m_@q_x, trim_t_@q_x)
-            )
-            
-            list(x_ = object@x_,
-                 t_ = object@t_,
-                 m_ = object@m_,
-                 table = data.frame(lt,
-                                    #discount = discount(object, 
-                                    #                    x_ = object@x_, )
-                                    benefit = c(rep(NA, ceiling(object@m_)), 
-                                                object@benefit))
-                 )
-            
-          }
+  function(object) {
+    lt <- trim_table(object, x_ = object@x_, t_ = object@t_, m_ = object@m_)        
+    print(
+      list(x_ = object@x_,
+           t_ = object@t_,
+           m_ = object@m_,
+           table_ = data.frame(
+                      show(lt),
+                      discount = discount(
+                                   object, 
+                                   x_ = object@x_, 
+                                   t_ = object@t_,
+                                   m_ = object@m_
+                                 ),
+                      benefit = c(rep(NA, ceiling(object@m_)), 
+                                  object@benefit)
+                    )
+      )
+    )
+  }
 )
