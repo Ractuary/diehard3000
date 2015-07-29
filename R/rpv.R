f6c9527a36111488f2b8d352347a3b6ac99cccfe
merlinoa <merlinoa88@gmail.com>
2015-07-29 14:25:35 -0400

    seperating annuity and life simulations into seperate functions

--- R/rpv.R
+++ R/rpv.R
@@ -1,76 +1,83 @@
-#' rpv
+#' rpv_life
 #' 
 #' present value of \code{rdeath} simulation
 #' 
 #' @export
-setGeneric("rpv", 
+setGeneric("rpv_life", 
            #valueClass = "numeric",
-           function(object, n, benefit_type = "life") {
-             standardGeneric("rpv")
+           function(object, n) {
+             standardGeneric("rpv_life")
            }
 )
 
 
 
-#' rpv
+#' rpv_life
 #' 
-#' Simulates the present value of life insurance or life contingent
-#' annuity payments for one Insuree object
+#' Simulates the present value of a single benefit life insurance payment
 #' 
 #' @param object object of class Insuree
 #' @param n number of observations
-#' @param benefit_type character string of either "life" or "annuity"
 #' 
 #' @export
 #' @examples
-#' rpv(object = Insuree(m_ = .5, benefit = c(1,1, 1, 1)), n = 5, benefit_type = "annuity")
-#' rpv(object = Insuree(x_ = 2, t_ = 3, benefit = c(1, 1, 1, 1), m_ = 0.3), n = 5)
-#' rpv(object = Insuree(x_ = 2.2, t_ = 3.4, benefit = c(1, 1, 1, 1), m_ = 0.3), n = 5, benefit_type = "annuity")
-#' rpv(object = Insuree(x_ = 2.48, t_ = 3.57, benefit = c(1, 1, 1, 1, 1), m_ = 0), n = 5)
-setMethod("rpv", signature("Insuree"), function(object, n, benefit_type = "life") {
+#' rpv_life(object = Insuree(x_ = 2, t_ = 3, benefit_t = c(1, 1, 1), benefit_value = c(3, 2, 1), m_ = 0.3), n = 5)
+#' rpv_life(object = Insuree(x_ = 2.48, t_ = 3.57, benefit_t = c(1, 1, 1, 0.57), benefit_value = c(2, 2, 2, 2), m_ = 3), n = 5)
+setMethod("rpv_life", signature("Insuree"), function(object, n) {
   
-  stopifnot(benefit_type %in% c("life", "annuity"))
   # simulate deaths
   deaths <- rdeath(object, n = n)
-  pv <- deaths[["death_table"]]
+  tod <- deaths[["death_t"]]
   
   # returns vector of discount factors
-  discount <- discount(object, x_ = object@x_, t_ = object@t_, m_ = object@m_)
-  # convert 1s to 0s if annuity
-  if (identical(benefit_type, "annuity")) {
-    # set insuree time of death to t_ + m_ if insuree did not die
-    tod <- deaths[["death_t"]]
-    tod[is.na(tod)] <- ceiling(object@x_ %% 1 + object@t_ + object@m_) + 1
-    
-    # change death_table to 1s for years insuree survives
-    for (j in seq_along(tod)) {
-      pv[1:tod[j], j] <- 1
-    }
-  }
-  
-  # if death in defferal period (i.e. x_ to x_ + m_)
-  # set present value of benefit to 0
-  if (object@m_ > 0) {
-    # set all deaths in deferral period equal to 0
-    pv[1:ceiling((object@x_ %% 1) + object@m_), ] <- 0
-    # set all deaths in term period equal to the applicable benefit value
-    if (benefit_type == "life") {
-      pv[ceiling((object@x_ %% 1) + object@m_ + 1):nrow(pv), ] <- 
-        pv[ceiling((object@x_ %% 1) + object@m_ + 1):nrow(pv), ] * object@benefit
-    } else {
-      # for annuities we only pay the portion of the annuity equal
-      # to the portion of the year in the term
-      pv[ceiling((object@x_ %% 1) + object@m_ + 1):nrow(pv), ] <- 
-        pv[ceiling((object@x_ %% 1) + object@m_ + 1):nrow(pv), ] * 
-        object@benefit * deaths$t[ceiling((object@x_ %% 1) + object@m_ + 1):nrow(pv)]
-    }
-  } else {
-    pv <- pv * object@benefit
-  }
-  
-  
-  pv <- apply(pv, 2, function(j) j * discount)
+  discount_table <- data.frame(
+    t_cume = cumsum(deaths$t),
+    discount = discount(object, x_ = object@x_, t_ = object@t_, m_ = object@m_)
+  )
+  # find applicable discount amount
+  discount <- sapply(tod, function(x) ifelse(is.na(x), NA, discount_table$discount[discount_table$t_cume == x]))
+  # find applicable benefit amount
+  tod[tod < object@m_] <- NA
+  benefit <- object@benefit_value[findInterval(tod, cumsum(c(object@m_, object@benefit_t)))]
+
+  # set all deaths in term period equal to the applicable benefit value
+  # function output
   list(deaths,
        discount = discount,
-       pv = apply(pv, 2, sum))
+       benefit = benefit,
+       pv = discount * benefit
+  )
 })
+
+#' rpv_annuity
+#'
+#' simulate the present value of life contingent
+#' annuity payments for one Insuree
+#' 
+#' @param object object of class Insuree
+#' @param n number of observations
+#' 
+#' @export
+#' @examples
+#' rpv_annuity(object = Insuree(m_ = .5, benefit = c(1,1, 1, 1)), n = 5)
+#' 
+#' 
+#setMethod("rpv_life", signature("Insuree"), function(object, n) {
+#  
+#  # simulate deaths
+#  deaths <- rdeath(object, n = n)
+#  pv <- deaths[["death_table"]]
+#  # convert 1s to 0s if annuity
+#  # set insuree time of death to t_ + m_ if insuree did not die
+#  tod <- deaths[["death_t"]]
+#  tod[is.na(tod)] <- ceiling(object@x_ %% 1 + object@t_ + object@m_) + 1
+#  pv[1:ceiling((object@x_ %% 1) + object@m_), ] <- 0
+#  # change death_table to 1s for years insuree survives
+#  for (j in seq_along(tod)) {
+#    pv[1:tod[j], j] <- 1
+#  }
+#  
+#  pv[ceiling((object@x_ %% 1) + object@m_ + 1):nrow(pv), ] <- 
+#    pv[ceiling((object@x_ %% 1) + object@m_ + 1):nrow(pv), ] * 
+#    object@benefit * deaths$t[ceiling((object@x_ %% 1) + object@m_ + 1):nrow(pv)]
+#})
