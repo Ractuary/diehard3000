46b6277c28a7ef4a8c0d7811eedf254ed10679c0
merlinoa88@gmail.com <merlinoa88@gmail.com>
2015-07-29 21:11:27 -0400

    added random uniform simulation for time of death during year of death

--- R/sim.R
+++ R/sim.R
@@ -33,14 +33,31 @@ setMethod("rdeath", signature("Insuree"), function(object, n) {
   death_probs <- tp_x8q_x(lt)
   
   # run the simulation
+  # this simulation outputs the time of death for each simulation
+  # as a column in the matrix
   deaths <- rmultinom(n = n, size = 1, prob = death_probs)
   deaths <- deaths[-nrow(deaths), , drop = FALSE]
   
+  # now that we have the period of death we will use a random draw from the
+  # uniform distribution to simulate the time of death during the period (e.g. year)
+  death_interval <- apply(deaths, 2, function(l) ifelse(sum(l) > 0, object@t[l > 0], NA))
+  death_time <- lapply(death_interval, function(y) runif(n = 1, min = 0, max = y))
+  death_time <- unlist(death_time)
+  death_time[is.nan(death_time)] <- NA
+  
+  # we now have the time of death within the death interval
+  # now we will find the total time leading up to that interval and add
+  # that to the time lived during the interval (which we just calculated above)
   t_s <- cumsum(lt@t)
+  full_periods_lived <- apply(deaths, 2, function(l) {
+    ifelse(sum(l) > 0, t_s[which(l > 0) - 1], NA)
+  })
+  death_time <- full_periods_lived + death_time
+  
   # return simulation output
   list(Insuree = object,
        death_table = deaths,
-       death_t = apply(deaths, 2, function(l) ifelse(sum(l) > 0, t_s[l > 0], NA)),
+       death_t = death_time,
        probs_death = death_probs,
        t = lt@t,
        x = lt@x)
