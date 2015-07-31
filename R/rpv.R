26cb49ee3b998c36c663fe20094b78a761ba8761
merlinoa <merlinoa88@gmail.com>
2015-07-31 14:53:09 -0400

    getting closer on rpv_annuity

--- R/rpv.R
+++ R/rpv.R
@@ -14,15 +14,26 @@ setGeneric("rpv_life",
 
 #' rpv_life
 #' 
-#' Simulates the present value of a single benefit life insurance payment
+#' Simulates the present value of the life insurance benefit for
+#' an object of class \code{Insuree}
 #' 
 #' @param object object of class Insuree
 #' @param n number of observations
 #' 
 #' @export
 #' @examples
-#' rpv_life(object = Insuree(x_ = 2, t_ = 3, benefit_t = c(1, 1, 1), benefit_value = c(3, 2, 1), m_ = 0.3), n = 5)
-#' rpv_life(object = Insuree(x_ = 2.48, t_ = 3.57, benefit_t = c(1, 1, 1, 0.57), benefit_value = c(2, 2, 4, 2), m_ = 3), n = 5)
+#' rpv_life(object = Insuree(x_ = 2, 
+#'                           t_ = 3, 
+#'                           benefit_t = c(1, 1, 1), 
+#'                           benefit_value = c(3, 2, 1), 
+#'                           m_ = 0.3), 
+#'          n = 5)
+#' rpv_life(object = Insuree(x_ = 2.48, 
+#'                           t_ = 3.57, 
+#'                           benefit_t = c(1, 1, 1, 0.57), 
+#'                           benefit_value = c(2, 2, 4, 2), 
+#'                           m_ = 3), 
+#'          n = 5)
 setMethod("rpv_life", signature("Insuree"), function(object, n) {
   
   # simulate deaths
@@ -76,18 +87,41 @@ setGeneric("rpv_annuity",
 #' 
 #' @export
 #' @examples
-#' rpv_annuity(object = Insuree(m_ = .5, benefit = c(1,1, 1, 1)), n = 5)
+#' rpv_annuity(object = Insuree(x_ = 2.48, 
+#'                              t_ = 3.57, 
+#'                              benefit_t = c(1, 1, 1, 0.57), 
+#'                              benefit_value = c(2, 2, 4, 2), 
+#'                              m_ = 3), 
+#'             n = 5)
 setMethod("rpv_annuity", signature("Insuree"), function(object, n) {
   
   # simulate deaths
   deaths <- rdeath(object, n = n)
-
   tod <- deaths[["death_t"]]
+  
   # find all possible death and benefit intervals
+  # we need all possible intervals so we can discount each interval for
+  # at the appropriate benefit amount
   inters <- sort(unique(c(cumsum(deaths$t), cumsum(c(object@m_, object@benefit_t)))))
+  inters <- unique(inters)
   # identify benefit amount in each interval
   benefit_annual <- object@benefit_value[findInterval(inters, cumsum(c(object@m_, object@benefit_t)))]
-  benefit_pro_rata <- c(diff(inters), NA) * benefit
-  #benefit_final <- benefit[findInterval(tod, inters)]
-  #TODO: make new life table with prorate benefits and new t and x values for discount function
+  benefit_pro_rata <- c(diff(inters[inters >= object@m_]), NA) * benefit_annual
+  
+  # TODO: need inters segmented i for new LifeTable that will be used to calculate discount
+  #discount <- lapply(tod, function(x) discount(object, t_ = )) 
+  
+  # find applicable benefit amount
+  #tod[tod < object@m_] <- NA
+  #benefit <- object@benefit_value[findInterval(tod, cumsum(c(object@m_, object@benefit_t)))]
+  
+  # set all deaths in term period equal to the applicable benefit value
+  # function output
+  #pv = discount * benefit
+  #pv[is.na(pv)] <- 0
+  #list(deaths,
+  #     discount = discount,
+  #     benefit = benefit,
+  #     pv = pv
+  #)
 })
