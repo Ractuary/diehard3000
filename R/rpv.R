0249a32d8fe23cf2380a10ab6758bdb4fefe0f6b
merlinoa <merlinoa88@gmail.com>
2015-07-30 20:41:45 -0400

    implemented new discount_death() function and now returning pv = 0 for simulations in which Insuree survives

--- R/rpv.R
+++ R/rpv.R
@@ -22,28 +22,20 @@ setGeneric("rpv_life",
 #' @export
 #' @examples
 #' rpv_life(object = Insuree(x_ = 2, t_ = 3, benefit_t = c(1, 1, 1), benefit_value = c(3, 2, 1), m_ = 0.3), n = 5)
-#' rpv_life(object = Insuree(x_ = 2.48, t_ = 3.57, benefit_t = c(1, 1, 1, 0.57), benefit_value = c(2, 2, 2, 2), m_ = 3), n = 5)
+#' rpv_life(object = Insuree(x_ = 2.48, t_ = 3.57, benefit_t = c(1, 1, 1, 0.57), benefit_value = c(2, 2, 4, 2), m_ = 3), n = 5)
 setMethod("rpv_life", signature("Insuree"), function(object, n) {
   
   # simulate deaths
   deaths <- rdeath(object, n = n)
   tod <- deaths[["death_t"]]
   
-  # returns vector of discount factors
-  discount_all <- lapply(tod, function(v) {
-                           min(discount(object, 
-                                    x_ = object@x_, 
-                                    t_ = object@t_, 
-                                    m_ = object@m_, 
-                                    death_time = v))
-                    })
   # find applicable discount amount
   discount <- c()
   for (j in seq_along(tod)) {
-    discount[j] <- if(is.na(tod[j])) {
-      NA
+    if(is.na(tod[j])) {
+      discount[j] <- NA
     } else {
-      discount_all[[j]][length(discount_all[[j]])] 
+      discount[j] <- discount_death(object, death_time = tod[j]) 
     }
   }
   # find applicable benefit amount
@@ -52,10 +44,12 @@ setMethod("rpv_life", signature("Insuree"), function(object, n) {
 
   # set all deaths in term period equal to the applicable benefit value
   # function output
+  pv = discount * benefit
+  pv[is.na(pv)] <- 0
   list(deaths,
        discount = discount,
        benefit = benefit,
-       pv = discount * benefit
+       pv = pv
   )
 })
 
