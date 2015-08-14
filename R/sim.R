a8d215096af8ac7a8e485bc4ec42e093d3ac8017
merlinoa <merlinoa88@gmail.com>
2015-08-14 18:08:46 -0400

    improved comments, fixed warning message from runif, and improved documentation

--- R/sim.R
+++ R/sim.R
@@ -15,11 +15,12 @@ setGeneric("rdeath",
 
 #' rdeath
 #' 
-#' simulated for \code{Z_x}.  The
+#' simulated time of death for an object of class \code{Insuree}.  The
 #' simulation is in accordance with the multinomial distribution described
-#' by the LifeTable object.
+#' by the LifeTable object.  Uniform time of death is assumed between x values
+#' indicated by the LifeTable object.
 #' 
-#' @param object object of class Z_x
+#' @param object object of class Insuree
 #' @param n number of observations
 #' 
 #' @export
@@ -32,18 +33,25 @@ setMethod("rdeath", signature("Insuree"), function(object, n) {
   lt <- trim_table(object, x_ = object@x_, t_ = object@t_, m_ = object@m_)
   death_probs <- tp_x8q_x(lt)
   
-  # run the simulation
-  # this simulation outputs the time of death for each simulation
-  # as a column in the matrix
+  ## Simulate the time of death
+  # The `rmultinom` simulation outputs a matrix.
+  # Each column represents a simulated individual where a 0 
+  # indicates the indivual survived the corresponding lt@t 
+  # period and a 1 indicates the individual died during the corresponding
+  # lt@t period. If the 1 is in the final element of the column than
+  # the individual survived the entire insured term of the policy.
   deaths <- rmultinom(n = n, size = 1, prob = death_probs)
+  # remove final row (final row of 1 indicates survival)
   deaths <- deaths[-nrow(deaths), , drop = FALSE]
   
-  # now that we have the period of death we will use a random draw from the
-  # uniform distribution to simulate the time of death during the period (e.g. year)
-  death_interval <- apply(deaths, 2, function(l) ifelse(sum(l) > 0, object@t[l > 0], NA))
-  death_time <- lapply(death_interval, function(y) runif(n = 1, min = 0, max = y))
-  death_time <- unlist(death_time)
-  death_time[is.nan(death_time)] <- NA
+  ## now that we have the t of death we will use a random draw from the
+  ## uniform distribution to simulate the time of death during the t.  This
+  ## allows us to simulate the exact time of death.
+  # Determine length of t in which individual died
+  death_t_length <- apply(deaths, 2, function(l) ifelse(sum(l) > 0, object@t[l > 0], NA))
+  # Run random uniform simulation over t of death
+  death_t_time <- lapply(death_t_length, function(y) ifelse(is.na(y), NA, runif(n = 1, min = 0, max = y)))
+  death_t_time <- unlist(death_t_time)
   
   # we now have the time of death within the death interval
   # now we will find the total time leading up to that interval and add
@@ -52,7 +60,7 @@ setMethod("rdeath", signature("Insuree"), function(object, n) {
   full_periods_lived <- apply(deaths, 2, function(l) {
     ifelse(sum(l) > 0, t_s[which(l > 0) - 1], NA)
   })
-  death_time <- full_periods_lived + death_time
+  death_time <- full_periods_lived + death_t_time
   
   # return simulation output
   list(Insuree = object,
