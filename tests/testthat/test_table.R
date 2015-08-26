e618acbb1dbc7ad58ba8ca059c7738b3e0e267f0
merlinoa <merlinoa88@gmail.com>
2015-08-26 19:34:25 -0400

    removed obselete tests

--- tests/testthat/test_table.R
+++ tests/testthat/test_table.R
@@ -1,8 +1,8 @@
 library(insuree)
 context("expected")
 
-test_that("expected returns correct value", {
-  expect_equal(round(expected(LifeTable(), x_ = 2), 6), 5.264875)
-  expect_equal(expected(LifeTable(), x_ = 0, t_ = 1), 0.975)
-  expect_equal(expected(LifeTable(), x_ = 0, t_ = 1, m_ = 1), 0.97)
+l <- LifeTable()
+
+test_that("p_x function", {
+  expect_equal(p_x(l, x_ = 2, t_ = 1), 0.93)
 })
