8a93e41c82220f537e16556b0e549bd6579a415a
merlinoa88@gmail.com <merlinoa88@gmail.com>
2015-09-01 10:11:38 -0400

    corrected p_x formula

--- R/p-x.R
+++ R/p-x.R
@@ -28,8 +28,7 @@ setGeneric("p_x",
 #' p_x(LifeTable(), x_ = 2.5, t_ = 2.5)
 #' p_x(LifeTable(), x_ = 2.5, t_ = 3.5)
 setMethod("p_x", signature("LifeTable"), function(object, x_ = min(object@x), t_ = 1) {
-  stopifnot(length(t_) == 1)
-  stopifnot(length(x_) == 1)
+  stopifnot(identical(length(t_), length(x_), 1))
   
   # remove all q_x rows less than x argument
   q_x <- object@q_x[index(object@x, x_ = x_, m_t_ = t_)]
@@ -39,10 +38,11 @@ setMethod("p_x", signature("LifeTable"), function(object, x_ = min(object@x), t_
   t_new <- diff(.x)
   
   # calculate new q_x values
+  # partial/fractional years use uniform force of mortality
   .q_x <- c()
   for (k in seq_along(q_x)) {
     .q_x[k] <- (q_x[k] * (t_new[k] / t_old[k])) / 
-                  (1 - q_x[k] * (1 - (min(.x[k + 1], x_old[k + 1]) - .x[k]) / t_new[k]))
+                  (1 - q_x[k] * (1 - t_new[k] / (min(.x[k + 1], x_old[k + 1]) - x_old[k])))
   }
   
   # calculate probability of survival: t_p_x
