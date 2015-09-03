32c8dc0b3cb18db415d16235398e04a09108012a
merlinoa <merlinoa88@gmail.com>
2015-09-03 17:50:39 -0400

    created plot hist and summary methods for rpv_Pool

--- R/plot.R
+++ R/plot.R
@@ -1,8 +1,9 @@
 #' hist.rpv_Insuree
 #' 
-#' plot a histogram of the present value from a rpv_life() simulation
+#' plot a histogram of the present value from a rpv() simulation of the
+#' \code{Insuree} class.
 #' 
-#' @param object rpv_life object
+#' @param object rpv_Insuree object
 #' @param ... other arguments
 #' 
 #' @import ggplot2
@@ -29,7 +30,7 @@ hist.rpv_Insuree <- function(object, ...) {
 #' the present value of death benefits from a rpv_life() 
 #' simulation
 #' 
-#' @param object rpv_life object
+#' @param object rpv_Insuree object
 #' @param ... other arguments
 #' 
 #' @import ggplot2
@@ -48,4 +49,63 @@ plot.rpv_Insuree <- function(object, ...) {
     xlab("Present Value of Death Benefit") +
     ylab("F(x)") +
     ggtitle("rpv_life() Empirical Cumulative Distribution")
+}
+
+#' hist.rpv_Pool
+#' 
+#' plot a histogram of the present value from a rpv() simulation of the
+#' \code{Pool} class
+#' 
+#' @param object rpv_Pool object
+#' @param ... other arguments
+#' 
+#' @import ggplot2
+#' @import scales
+#' 
+#' @export
+#' @examples
+#' test <- rpv(object = Pool(), 
+#'             n = 500,
+#'             interest = 0.04)
+#' hist(test)
+hist.rpv_Pool <- function(object, ...) {
+  data <- summary.rpv_Pool(object)
+  losses <- apply(data, 1, sum)
+  losses <- as.data.frame(pv = losses)
+  ggplot2::ggplot(losses, aes(x = pv)) +
+    geom_histogram(fill = "white", colour = "black") +
+    scale_x_continuous(labels = dollar) +
+    xlab("Present Value of Death Benefits") +
+    ylab("# of Observations") +
+    ggtitle(paste0("rpv() Histogram"))
+}
+
+#' plot.rpv_Pool
+#' 
+#' plot the empirical cumulative distribution of 
+#' the present value of death benefits from a rpv() 
+#' simulation of the \code{Pool} class.
+#' 
+#' @param object rpv_Pool object
+#' @param ... other arguments
+#' 
+#' @import ggplot2
+#' @import scales
+#' 
+#' @export
+#' @examples
+#' test <- rpv(object = Pool(), 
+#'             n = 500,
+#'             interest = 0.04)
+#' plot(test)
+plot.rpv_Insuree <- function(object, ...) {
+  data <- summary.rpv_Pool(object)
+  losses <- apply(data, 1, sum)
+  losses <- as.data.frame(pv = losses)
+  ggplot2::ggplot(data.frame(pv = object$pv), aes(x = pv)) +
+    stat_ecdf() +
+    scale_x_continuous(labels = dollar) +
+    xlab("Present Value of Death Benefits") +
+    ylab("F(x)") +
+    ggtitle("rpv() Empirical Cumulative Distribution")
 }
--- R/summary.R
+++ R/summary.R
@@ -1,16 +1,41 @@
-#' summary.rpv_life
+#' summary.rpv_Insuree
 #' 
-#' summary of rpv_life() simulation
+#' summary of rpv() simulation of class \code{Insuree}
 #' 
-#' @param object rpv_life object
+#' @param object \code{rpv_Insuree} object
 #' @param ... other arguments
 #' 
 #' @export
 #' @examples
-#' test <- rpv_life(object = Insuree(), 
-#'                  n = 500,
-#'                  interest = 0.04)
+#' test <- rpv(object = Insuree(), 
+#'             n = 500,
+#'             interest = 0.04)
 #' summary(test)
-summary.rpv_life <- function(object, ...) {
+summary.rpv_Insuree <- function(object, ...) {
   object
+}
+
+#' summary.rpv_Pool
+#' 
+#' summary of rpv() simulation of the \code{Pool} class
+#' 
+#' @param object \code{rpv_Pool} object
+#' @param ... other arguments
+#' 
+#' @export
+#' @examples
+#' test <- rpv(object = Pool(), 
+#'             n = 500,
+#'             interest = 0.04)
+#' summary(test)
+summary.rpv_Pool <- function(object, ...) {
+  pv <- lapply(object, function(x) x$pv)
+  pv <- as.data.frame(pv)
+  
+  names2 <- vector(mode = "character", length = length(object))
+  for (k in seq_along(object)) {
+    names2[k] <- paste0("Insuree", k)
+  }
+  names(pv) <- names2
+  pv
 }
