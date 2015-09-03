1a359f419688a924ee2a02f48a400012e7a0ef5c
merlinoa88@gmail.com <merlinoa88@gmail.com>
2015-09-03 10:25:55 -0400

    fixed slow for loop and added stop for when cir random walk dips below zero

--- R/interest.R
+++ R/interest.R
@@ -10,9 +10,10 @@
 #' 
 #' @export
 #' @examples 
+#' rcir(n = 10, r = 0.01, b = 0.04, a = 0.5, s = 0.07)
 #' rcir(n = 10, r = 0.01, b = 0.04, a = 1, s = 0.05)
 rcir <- function(n, r, b, a, s) {  
-  output <- c()
+  output <- vector(mode = "numeric", length = n)
   output[1] <- cir(r, b, a, s)
   for (i in 2:n) {
     output[i] <- cir(output[i - 1], b, a, s)
@@ -21,5 +22,33 @@ rcir <- function(n, r, b, a, s) {
 }
 
 cir <- function(r, b, a, s) {
-  r + a*(b - r) + s * sqrt(r) * rnorm(1, 0, 1)
+  stopifnot(r >= 0) # CIR walk does not support negative interest rates
+  max(r + a*(b - r) + s * sqrt(r) * rnorm(1, 0, 1), 0)
+}
+
+#' discount
+#'
+#' discount single benefit to present value
+#' 
+#' @param interest vector of annual interest rates
+#' @param benefit_time the time (from x_) of death
+#' 
+#' @export
+#' @examples
+#' discount(0.04, death_time = 1.01)
+#' discount(0.04, death_time = 0.8)
+discount <- function(interest, benefit_time = NA) {
+  if (is.na(benefit_time)) return(NA_real_)
+  
+  # repeat interest vector until it has length == ceiling(benefit_time)
+  # if the interest vector is not long enough
+  if (length(interest) <= benefit_time) {
+    interest <- rep(interest, length.out = ceiling(benefit_time))
+  }
+  
+  # find applicable trend factors
+  trend <- 1 + interest[1:ceiling(benefit_time)]
+  trend[length(trend)] <- trend[length(trend)] ^ (benefit_time %% 1)
+  # calculate trend for given benefit time
+  1 / prod(trend)
 }
