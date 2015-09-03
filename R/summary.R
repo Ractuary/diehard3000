323ad80b6dcd1b8d3d8087750a934c4d2e4dbfc5
merlinoa88@gmail.com <merlinoa88@gmail.com>
2015-09-03 10:25:11 -0400

    set up summary method

---
+++ R/summary.R
@@ -0,0 +1,15 @@
+#' summary.rpv_life
+#' 
+#' summary of rpv_life() simulation
+#' 
+#' @param object rpv_life object
+#' 
+#' @export
+#' @examples
+#' test <- rpv_life(object = Insuree(), 
+#'                  n = 500,
+#'                  interest = 0.04)
+#' summary(test)
+summary.rpv_life <- function(object) {
+  object
+}
